cdfe8dcab610f19c93db7c98c623471577ed382a
Justus <galaxycheesemc@gmail.com>
2023-01-12 08:53:24 -0700

    Installed packages, moved starter files into repo

---
+++ index.js
@@ -0,0 +1,35 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = requires("./utils/generateMarkdown.js")
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        question: "What is the title?"
+    },
+    {
+        quesiton: "What is the description of your project?"
+    },
+    {
+        quesiton: "What is the link to your deployed application?"
+    }, 
+    {
+        question:
+    },
+    {
+
+    }];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+
+}
+
+// Function call to initialize app
+init();
